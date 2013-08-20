396b43ab9a005f315ffb6fc506b389d040767113
MEUS DOCUMENTOS <MEUS DOCUMENTOS@192.168.0.105>
2013-08-20 17:06:33 -0300

    Criação dos pacotes do projeto e da classe Principal - responsável por executar a aplicação.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/app/Principal.java
@@ -0,0 +1,16 @@
+
+
+package br.senai.sc.app;
+
+/**
+ * Classe responsável por executar a aplicação
+ * @author Rosicléia Frasson
+ * @version 1.0 20/08/2013
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
