fcc60c6674a2beb5696fadafd57a92d794c938a3
rosicleia.souza <rosicleia.souza@10.1.49.195>
2013-08-26 17:12:21 -0300

    Criação dos pacotes necessários para a aplicação.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/model/negocio/Pessoa.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.negocio;
+
+/**
+ *
+ * @author rosicleia.souza
+ */
+public class Pessoa {
+    
+}
