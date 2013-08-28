896294391711e6460e845df3cd511b5104be6899
gabriel_arsenio <gabriel_arsenio@10.1.48.225>
2013-08-28 08:58:58 -0300

    Criado os métodos equals e toString.

--- ProjetoConfeccao3fase/src/br/senai/sc/model/negocio/CategoriaColecao.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/model/negocio/CategoriaColecao.java
@@ -1,5 +1,7 @@
 package br.senai.sc.model.negocio;
 
+import java.util.Objects;
+
 /**
  * Classe CategoriaColecao * A possui atributos para cadastrar categorias de
  * coleção
@@ -13,6 +15,42 @@ public class CategoriaColecao {
     private String nome;
     private String descricao;
 
+    @Override
+    public String toString() {
+        return "Categoria de coleção" + ""
+                + "\nCódigo da categoria: " + cod + ""
+                + "\nNome: " + nome + ""
+                + "\nDescrição: " + descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CategoriaColecao other = (CategoriaColecao) obj;
+        if (this.cod != other.cod) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.descricao, other.descricao)) {
+            return false;
+        }
+        return true;
+    }
+
+    //Getters e Setters
     public int getCod() {
         return cod;
     }
@@ -36,4 +74,5 @@ public class CategoriaColecao {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+    //Fim Getters e Setters
 }
