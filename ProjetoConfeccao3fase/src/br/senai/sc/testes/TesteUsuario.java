ad7370af778d576c2d242d5074c1f36fb50feffe
larissa_mezari <larissa_mezari@10.1.49.153>
2013-08-28 16:12:06 -0300

    Classe usuário criada com os atrubutos, getters e setters, equals, toString.
    Classe teste criada e instanciada.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/TesteUsuario.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.testes;
+
+import br.senai.sc.model.negocio.Usuario;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author larissa_mezari
+ */
+public class TesteUsuario {
+    
+    public static void main(String[] args) {
+        
+        //Instancia do usuario 1  
+     Usuario u1 = new Usuario();
+    u1.setCod(Integer.parseInt(JOptionPane.showInputDialog("Informe o código: " + u1.getCod())));
+    u1.setCod_usuario(Integer.parseInt(JOptionPane.showInputDialog("Informe o cógigo do usuario: ")));
+    u1.setNome(JOptionPane.showInputDialog("Digite seu nome: "));
+    u1.setCpf(JOptionPane.showInputDialog("Informe o cpf: "));
+    u1.setDataCadastro(JOptionPane.showInputDialog("Informe a data de cadastro: "));
+    u1.setDataNascimento(Integer.parseInt(JOptionPane.showInputDialog("Informe a data de nascimento: ")));
+    u1.setEmail(JOptionPane.showInputDialog("Informe seu E-mail: "));
+    u1.setEndereco(JOptionPane.showInputDialog("Informe o endereço: "));
+    u1.setLogin(JOptionPane.showInputDialog("Entre com o login: "));
+    u1.setSenha(JOptionPane.showInputDialog("Digite sua senha: "));
+    
+    
+        //Instancia do usuario 2
+     Usuario u2 = new Usuario();
+    u2.setCod(Integer.parseInt(JOptionPane.showInputDialog("Informe o código: " + u1.getCod())));
+    u2.setCod_usuario(Integer.parseInt(JOptionPane.showInputDialog("Informe o cógigo do usuario: ")));
+    u2.setNome(JOptionPane.showInputDialog("Digite seu nome: "));
+    u2.setCpf(JOptionPane.showInputDialog("Informe o cpf: "));
+    u2.setDataCadastro(JOptionPane.showInputDialog("Informe a data de cadastro: "));
+    u2.setDataNascimento(Integer.parseInt(JOptionPane.showInputDialog("Informe a data de nascimento: ")));
+    u2.setEmail(JOptionPane.showInputDialog("Informe seu E-mail: "));
+    u2.setEndereco(JOptionPane.showInputDialog("Informe o endereço: "));
+    u2.setLogin(JOptionPane.showInputDialog("Entre com o login: "));
+    u2.setSenha(JOptionPane.showInputDialog("Digite sua senha: "));
+    
+    }
+    
+}
