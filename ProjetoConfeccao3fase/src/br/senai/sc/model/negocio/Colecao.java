a34c5154c6cef4225c22c0b57ac74a602c3f92fb
gabriel_arsenio <gabriel_arsenio@10.1.48.225>
2013-08-28 09:17:37 -0300

    Criada a classe Colecao com os atributos codigo(int cod), estação(String estacao), ano(int ano), produtos da coleção(String prodColecao), público alvo(String pubAlvo) e funcionário responsável(funResponsavel) com seus respectivos getters e setters. Também criado o método equals.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/model/negocio/Colecao.java
@@ -0,0 +1,107 @@
+package br.senai.sc.model.negocio;
+
+import java.util.Objects;
+
+/**
+ * Classe Colecao
+ * Representa as informações de cada coleção
+ * 
+ * @version v1.0 28/08/2013
+ * @author Gabriel Arsênio
+ */
+public class Colecao {
+    
+    private int cod;
+    private String estacao;
+    private int ano;
+    private String prodColecao;
+    private String pubAlvo;
+    private String funResponsavel;
+
+    
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Colecao other = (Colecao) obj;
+        if (this.cod != other.cod) {
+            return false;
+        }
+        if (!Objects.equals(this.estacao, other.estacao)) {
+            return false;
+        }
+        if (this.ano != other.ano) {
+            return false;
+        }
+        if (!Objects.equals(this.prodColecao, other.prodColecao)) {
+            return false;
+        }
+        if (!Objects.equals(this.pubAlvo, other.pubAlvo)) {
+            return false;
+        }
+        if (!Objects.equals(this.funResponsavel, other.funResponsavel)) {
+            return false;
+        }
+        return true;
+    }    
+    
+    //Getters e Setters
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getEstacao() {
+        return estacao;
+    }
+
+    public void setEstacao(String estacao) {
+        this.estacao = estacao;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getProdColecao() {
+        return prodColecao;
+    }
+
+    public void setProdColecao(String prodColecao) {
+        this.prodColecao = prodColecao;
+    }
+
+    public String getPubAlvo() {
+        return pubAlvo;
+    }
+
+    public void setPubAlvo(String pubAlvo) {
+        this.pubAlvo = pubAlvo;
+    }
+
+    public String getFunResponsavel() {
+        return funResponsavel;
+    }
+
+    public void setFuncionarioResponsavel(String funResponsavel) {
+        this.funResponsavel = funResponsavel;
+    }   
+    //Fim Getters e Setters
+}
