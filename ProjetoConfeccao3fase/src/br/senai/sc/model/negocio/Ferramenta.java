35884a2760fcafc9c762cd95d61b0970929b7567
ariane_souza <ariane_souza@10.1.48.185>
2013-08-28 09:44:56 -0300

    Classe de representação da Ferramenta, com seus atributos, os métodos Set e Get, equals e toString.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/model/negocio/Ferramenta.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.negocio;
+
+import java.util.Objects;
+
+/**
+ * Classe de representação da ferramenta
+ * @version 1.0 28/08/2013
+ * @author ariane_souza
+ */
+public class Ferramenta {
+    //Atributos da classe
+    private String nome;
+    private String descricao;
+    private String nmFabricante;
+    private String dtCadastro;
+    private boolean status;
+
+    //Métodos Get e Set
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNmFabricante() {
+        return nmFabricante;
+    }
+
+    public void setNmFabricante(String nmFabricante) {
+        this.nmFabricante = nmFabricante;
+    }
+
+    public String getDtCadastro() {
+        return dtCadastro;
+    }
+
+    public void setDtCadastro(String dtCadastro) {
+        this.dtCadastro = dtCadastro;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    //Equals
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Ferramenta other = (Ferramenta) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.descricao, other.descricao)) {
+            return false;
+        }
+        if (!Objects.equals(this.nmFabricante, other.nmFabricante)) {
+            return false;
+        }
+        if (!Objects.equals(this.dtCadastro, other.dtCadastro)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        return true;
+    }
+    //toString
+    @Override
+    public String toString() {
+        return "Ferramenta: " + nome + 
+                "\nDescricao: " + descricao +
+                "\nNome do Fabricante: " + nmFabricante +
+                "\nData do Cadastro: " + dtCadastro +
+                "\nStatus: " + status;
+    }
+}
