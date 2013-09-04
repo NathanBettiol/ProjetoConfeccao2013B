b473d347c71d42432fef9c69fd2a7a56dc8e59c8
gabriel_arsenio <gabriel_arsenio@10.1.48.225>
2013-09-04 16:14:38 -0300

    Alterações feitas automaticamente depois da melhora nos padrões da classe Colecao.

--- ProjetoConfeccao3fase/src/br/senai/sc/testes/TesteColecao.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/TesteColecao.java
@@ -20,21 +20,21 @@ public class TesteColecao {
     public static void main(String[] args) {
         //Instância de Colecao 0
         Colecao c = new Colecao();
-        c.setCod(0);
-        c.setEstacao(JOptionPane.showInputDialog("Informe a estação dessa coleção"));
-        c.setAno(Integer.parseInt(JOptionPane.showInputDialog("Informe o ano dessa coleção")));
-        c.setPubAlvo(JOptionPane.showInputDialog("Informe o público alvo dessa coleção"));
-        c.setFunResponsavel(JOptionPane.showInputDialog("Informe o funcionário responsável por essa coleção"));
+        c.setCodColecao(0);
+        c.setEstacaoColecao(JOptionPane.showInputDialog("Informe a estação dessa coleção"));
+        c.setAnoColecao(Integer.parseInt(JOptionPane.showInputDialog("Informe o ano dessa coleção")));
+        c.setPubAlvoColecao(JOptionPane.showInputDialog("Informe o público alvo dessa coleção"));
+        c.setFunResponsavelColecao(JOptionPane.showInputDialog("Informe o funcionário responsável por essa coleção"));
 
         JOptionPane.showMessageDialog(null, c.toString());
 
         //Instância de Colecao 1
         Colecao c1 = new Colecao();
-        c1.setCod(1);
-        c1.setEstacao(JOptionPane.showInputDialog("Informe a estação dessa coleção"));
-        c1.setAno(Integer.parseInt(JOptionPane.showInputDialog("Informe o ano dessa coleção")));
-        c1.setPubAlvo(JOptionPane.showInputDialog("Informe o público alvo dessa coleção"));
-        c1.setFunResponsavel(JOptionPane.showInputDialog("Informe o funcionário responsável por essa coleção"));
+        c1.setCodColecao(1);
+        c1.setEstacaoColecao(JOptionPane.showInputDialog("Informe a estação dessa coleção"));
+        c1.setAnoColecao(Integer.parseInt(JOptionPane.showInputDialog("Informe o ano dessa coleção")));
+        c1.setPubAlvoColecao(JOptionPane.showInputDialog("Informe o público alvo dessa coleção"));
+        c1.setFunResponsavelColecao(JOptionPane.showInputDialog("Informe o funcionário responsável por essa coleção"));
 
         JOptionPane.showMessageDialog(null, c1.toString());
     }
