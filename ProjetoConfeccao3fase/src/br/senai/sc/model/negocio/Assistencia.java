74b2014f3b3cfef2a79e1971c1f0e31239b8e825
gabriel_arsenio <gabriel_arsenio@10.1.48.225>
2013-09-04 16:11:31 -0300

    Criada a classe Assistencia com seus respectivos atributos e métodos.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/model/negocio/Assistencia.java
@@ -0,0 +1,163 @@
+package br.senai.sc.model.negocio;
+
+import java.util.Objects;
+
+/**
+ * Classe Assistencia * Informações da assistência
+ *
+ * @version v1.0 04/09/2013
+ * @author Gabriel Arsênio
+ */
+public class Assistencia {
+
+    //Atributos
+    private int codAssistencia;
+    private String nomeAssistencia;
+    private String nomeFantasiaAssistencia;
+    private String cnpjAssistencia;
+//    private Endereco endereco; //Aguardando a classe endereco ser criada
+    private String razaoSocialAssistencia;
+    private String telefoneAssistencia;
+    private String emailAssistencia;
+    private String paginaWebAssistencia;
+    private String contatoAssistencia;
+
+    //Construtor default
+    public Assistencia() {
+    }
+
+    //Método toString
+    @Override
+    public String toString() {
+        return "\nAssistência"
+                + "\nCódigo: " + codAssistencia
+                + "\nNome: " + nomeAssistencia
+                + "\nNome Fantasia: " + nomeFantasiaAssistencia
+                + "\nCNPJ: " + cnpjAssistencia
+                + "\nRazao Social: " + razaoSocialAssistencia
+                + "\nTelefone: " + telefoneAssistencia
+                + "\nE-mail: " + emailAssistencia
+                + "\nPágina Web: " + paginaWebAssistencia
+                + "\nContato: " + contatoAssistencia;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+    
+    //Método equals
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Assistencia other = (Assistencia) obj;
+        if (this.codAssistencia != other.codAssistencia) {
+            return false;
+        }
+        if (!Objects.equals(this.nomeAssistencia, other.nomeAssistencia)) {
+            return false;
+        }
+        if (!Objects.equals(this.nomeFantasiaAssistencia, other.nomeFantasiaAssistencia)) {
+            return false;
+        }
+        if (!Objects.equals(this.cnpjAssistencia, other.cnpjAssistencia)) {
+            return false;
+        }
+        if (!Objects.equals(this.razaoSocialAssistencia, other.razaoSocialAssistencia)) {
+            return false;
+        }
+        if (!Objects.equals(this.telefoneAssistencia, other.telefoneAssistencia)) {
+            return false;
+        }
+        if (!Objects.equals(this.emailAssistencia, other.emailAssistencia)) {
+            return false;
+        }
+        if (!Objects.equals(this.paginaWebAssistencia, other.paginaWebAssistencia)) {
+            return false;
+        }
+        if (!Objects.equals(this.contatoAssistencia, other.contatoAssistencia)) {
+            return false;
+        }
+        return true;
+    }
+
+    //Getters e Setters
+    public int getCodAssistencia() {
+        return codAssistencia;
+    }
+
+    public void setCodAssistencia(int codAssistencia) {
+        this.codAssistencia = codAssistencia;
+    }
+
+    public String getNomeAssistencia() {
+        return nomeAssistencia;
+    }
+
+    public void setNomeAssistencia(String nomeAssistencia) {
+        this.nomeAssistencia = nomeAssistencia;
+    }
+
+    public String getNomeFantasiaAssistencia() {
+        return nomeFantasiaAssistencia;
+    }
+
+    public void setNomeFantasiaAssistencia(String nomeFantasiaAssistencia) {
+        this.nomeFantasiaAssistencia = nomeFantasiaAssistencia;
+    }
+
+    public String getCnpjAssistencia() {
+        return cnpjAssistencia;
+    }
+
+    public void setCnpjAssistencia(String cnpjAssistencia) {
+        this.cnpjAssistencia = cnpjAssistencia;
+    }
+
+    public String getRazaoSocialAssistencia() {
+        return razaoSocialAssistencia;
+    }
+
+    public void setRazaoSocialAssistencia(String razaoSocialAssistencia) {
+        this.razaoSocialAssistencia = razaoSocialAssistencia;
+    }
+
+    public String getTelefoneAssistencia() {
+        return telefoneAssistencia;
+    }
+
+    public void setTelefoneAssistencia(String telefoneAssistencia) {
+        this.telefoneAssistencia = telefoneAssistencia;
+    }
+
+    public String getEmailAssistencia() {
+        return emailAssistencia;
+    }
+
+    public void setEmailAssistencia(String emailAssistencia) {
+        this.emailAssistencia = emailAssistencia;
+    }
+
+    public String getPaginaWebAssistencia() {
+        return paginaWebAssistencia;
+    }
+
+    public void setPaginaWebAssistencia(String paginaWebAssistencia) {
+        this.paginaWebAssistencia = paginaWebAssistencia;
+    }
+
+    public String getContatoAssistencia() {
+        return contatoAssistencia;
+    }
+
+    public void setContatoAssistencia(String contatoAssistencia) {
+        this.contatoAssistencia = contatoAssistencia;
+    }
+    //Fim Getters e Setters
+}
