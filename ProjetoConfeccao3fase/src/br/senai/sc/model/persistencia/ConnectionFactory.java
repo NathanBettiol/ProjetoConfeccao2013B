281c38c0da123f02760221b49bb374a433840e97
wesley_s <wesley_s@10.1.48.224>
2013-09-04 14:54:55 -0300

    Adicionando classe para fazer conecção com banco de dados

--- ProjetoConfeccao3fase/src/br/senai/sc/model/persistencia/ConnectionFactory.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/model/persistencia/ConnectionFactory.java
@@ -1,10 +1,21 @@
-
 package br.senai.sc.model.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
- * @author rosicleia.souza
+ * @author wesley.s
  */
 public class ConnectionFactory {
-    
+
+    public static Connection getConnection()
+            throws SQLException {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/dbconfeccao", "root", "");
+        } catch (Exception e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
 }
