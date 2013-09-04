31fb5828678174e442cef78b98d64c890ccf752d
gabriel_arsenio <gabriel_arsenio@10.1.48.225>
2013-09-04 16:57:24 -0300

    Criada a classe de testes para a classe Assistencia.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/TesteAssistencia.java
@@ -0,0 +1,15 @@
+package br.senai.sc.testes;
+
+/**
+ * Classe TesteAssistencia * Instância de dois objetos da classe Assistencia 
+ * para testar sua funcionalidade.
+ *
+ * @version v1.0 04/09/2013
+ * @author Gabriel Arsênio
+ */
+public class TesteAssistencia {
+    
+    public static void main(String[] args) {
+        
+    }
+}
