26a54d899ea463d1da774dc0e0c700eb225fc770
bruna_zakrzeski <bruna_zakrzeski@10.1.48.122>
2013-09-04 16:00:43 -0300

    Teste Instrutror  - Bruna Zakrzeski.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/TesteInstrutor.java
@@ -0,0 +1,35 @@
+
+package br.senai.sc.testes;
+
+import br.senai.sc.model.negocio.Instrutor;
+import javax.swing.JOptionPane;
+
+public class TesteInstrutor {
+    
+    /**
+* PrincipalInstrutor
+* @version v1.0  28/08/2013
+* @author Bruna Zakrzeski
+*/
+    
+    public static void main(String[] args) {
+        
+        
+        //Terceira classe instanciada INSTRUTOR
+        Instrutor inst = new Instrutor();
+        
+        
+        inst.codTreinamento = Integer.parseInt(JOptionPane.showInputDialog("Digite o código: "));
+        inst.nmfuncionario = JOptionPane.showInputDialog("Digite o nome: ");
+        inst.treinamento = JOptionPane.showInputDialog("Digite o nome do treinamento: ");
+        
+        //Imprimi os valores no método toString das VARIAVEIS da classe INSTRUTOR
+        System.out.println(inst.toString());
+        
+        
+    }
+    
+}
+
+    
+
