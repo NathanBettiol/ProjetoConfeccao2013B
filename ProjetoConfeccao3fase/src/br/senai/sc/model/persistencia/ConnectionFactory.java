8a26391e2361225b179858e693a1382198b27bf7
ariane_souza <ariane_souza@10.1.49.220>
2013-09-09 16:46:52 -0300

    ConnectionFactory

--- ProjetoConfeccao3fase/src/br/senai/sc/model/persistencia/ConnectionFactory.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/model/persistencia/ConnectionFactory.java
@@ -2,7 +2,9 @@ package br.senai.sc.model.persistencia;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -10,12 +12,41 @@ import java.sql.SQLException;
  */
 public class ConnectionFactory {
 
-    public static Connection getConnection()
-            throws SQLException {
+    private static final String STR_CONEXAO = "jdbc:mysql://localhost:3306/";
+    private static final String DATABASE = "exemploJDBC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/dbconfeccao", "root", "");
+            return DriverManager.getConnection(STR_CONEXAO + DATABASE, USER, PASSWORD);
         } catch (Exception e) {
             throw new SQLException(e.getMessage());
         }
+    }
+
+    public static void closeConnection(Connection con) throws SQLException {
+        if (con != null) {
+            con.close();
+            System.out.println("Conexao Finalizada");
+        }
+
+    }
+
+    public static void closeConnection(Connection con, Statement st) throws SQLException {
+        if (st != null) {
+            st.close();
+            System.out.println("Statement finalizado");
+        }
+        closeConnection(con);
+    }
+
+    public static void closeConnection(Connection con, Statement st, ResultSet rs) throws SQLException {
+        if (rs != null) {
+            rs.close();
+            System.out.println("ResultSet finalizado");
+        }
+        closeConnection(con, st);
     }
 }
