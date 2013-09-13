14557bca9adaec3758ce1cc6361ca772baccdb6e
wesley_s <wesley_s@10.1.48.225>
2013-09-13 14:53:43 -0300

    Adicionando a classe responsável pela persistência da entidade transportadora na base de dados

--- ProjetoConfeccao3fase/src/br/senai/sc/model/persistencia/TransportadoraDaoJDBC.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/model/persistencia/TransportadoraDaoJDBC.java
@@ -2,7 +2,10 @@ package br.senai.sc.model.persistencia;
 
 import br.senai.sc.model.negocio.Transportadora;
 import br.senai.sc.persistencia.dao.TransportadoraDAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  * Classe responsável pela persistência da entidade transportadora na base de
@@ -13,8 +16,8 @@ import java.util.List;
 public class TransportadoraDaoJDBC implements TransportadoraDAO {
 
     private final String INSERT = "insert into transportadora(nmFantasia, "
-            + "razaoSocial, cnpj, endereco, telefone, contato, email, paginaWeb)"
-            + " values (?, ?, ?)";
+            + "razaoSocial, cnpj, endereco, telefone, contato, email, paginaWeb,"
+            + " dtCadastro) values (?, ?, ?)";
     private final String UPDATE = "update transportadora set nmFantasia = ?, "
             + "razaoSocial = ?, cnpj = ?, endereco = ?, telefone = ?, "
             + "contato = ?, email = ?, paginaWeb = ? where cod = ?";
@@ -23,9 +26,35 @@ public class TransportadoraDaoJDBC implements TransportadoraDAO {
     private final String LISTBYID = "select * from transportadora "
             + "where cod = ?";
 
+    /*
+     * Método que realiza a inserção de uma transportadora na base de dados
+     */
     @Override
     public boolean insert(Transportadora t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn;
+        try {
+            conn = ConnectionFactory.getConnection();
+            PreparedStatement pstm = conn.prepareStatement(INSERT);
+
+            pstm.setString(1, t.getNmFantasia());
+            pstm.setString(1, t.getRazaoSocial());
+            pstm.setString(1, t.getCnpj());
+            pstm.setString(1, t.getEndereco());
+            pstm.setString(1, t.getTelefone());
+            pstm.setString(1, t.getContato());
+            pstm.setString(1, t.getEmail());
+            pstm.setString(1, t.getPaginaWeb());
+            pstm.setDate(8, new java.sql.Date(t.getDtCadastro().getTime()));
+            pstm.execute();
+            JOptionPane.showMessageDialog(null, "Transação efetuada com "
+                    + "sucesso");
+            ConnectionFactory.closeConnection(conn, pstm);
+            return true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Não foi possível efetuar a "
+                    + "transação: " + e.getMessage());
+            return false;
+        }
     }
 
     @Override
