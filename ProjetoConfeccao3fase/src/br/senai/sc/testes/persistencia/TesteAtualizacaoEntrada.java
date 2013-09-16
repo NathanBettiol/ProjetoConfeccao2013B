39ed48154e9290628f1a8e351e5b50dd6e7815f7
nathan_bettiol <nathan_bettiol@10.1.48.222>
2013-09-16 16:44:57 -0300

    Adicionado a ligacao com o banco de dados e as operacoes de atualizacao , insercao , remocao e lista da classe entrada.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/persistencia/TesteAtualizacaoEntrada.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.testes.persistencia;
+
+import br.senai.sc.model.negocio.Entrada;
+import br.senai.sc.model.negocio.Pessoa;
+import br.senai.sc.model.persistencia.EntradaDaoJDBC;
+import br.senai.sc.persistencia.dao.EntradaDAO;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author nathan_bettiol
+ */
+public class TesteAtualizacaoEntrada {
+    
+public static void main(String[] args) {
+        
+        
+        Entrada e = new Entrada();
+        
+        e.setProduto(JOptionPane.showInputDialog("Informe o nome do produto: "));
+        String dataEmissao = JOptionPane.showInputDialog("Informe a data de Emissao: ");
+        try {
+            e.setDtEmissao(new SimpleDateFormat("dd/MM/yyyy").parse(dataEmissao));
+        } catch (Exception ex) {
+            System.out.println("Erro ao converter a data de Emissão");
+        }
+        e.setFuncionario(JOptionPane.showInputDialog("Digite o nome Funcionario: "));
+        e.setNrNotaFiscal(Integer.parseInt(JOptionPane.showInputDialog("Digite o numero da nota fiscal: ")));
+        e.setPreco(Double.parseDouble(JOptionPane.showInputDialog("Digite o preço: ")));
+        e.setQtProduto(Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade do produto: ")));
+        
+        e.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Informe o "
+                + "id do produto a ser alterado")));
+        EntradaDAO dao = new EntradaDaoJDBC();
+        dao.update(e);
+    }
+}
