7aea7a9533ce2f37e782c2a3c020b08e8cd1fdb7
ariane_souza <ariane_souza@10.1.49.220>
2013-09-16 14:38:16 -0300

    Cria classe TesteConexaoFerramenta

--- ProjetoConfeccao3fase/src/br/senai/sc/testes/persistencia/TesteConexaoFerramenta.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/persistencia/TesteConexaoFerramenta.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.senai.sc.testes;
+package br.senai.sc.testes.persistencia;
 
 import br.senai.sc.model.negocio.Ferramenta;
 import br.senai.sc.model.persistencia.FerramentaDaoJDBC;
