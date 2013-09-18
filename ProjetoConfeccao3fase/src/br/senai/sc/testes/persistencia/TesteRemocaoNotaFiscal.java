db15b8f98b90054e1ee6719fd987fe06c05e169f
elivelton_sipriano <elivelton_sipriano@10.1.49.140>
2013-09-18 13:51:01 -0300

    adicionando a classe TesteRemocaoNotaFiscal

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/persistencia/TesteRemocaoNotaFiscal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.testes.persistencia;
+
+import br.senai.sc.model.persistencia.NotaFiscalDaoJDBC;
+import br.senai.sc.persistencia.dao.NotaFiscalDAO;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author elivelton_sipriano
+ */
+public class TesteRemocaoNotaFiscal {
+
+    public static void main(String[] args) {
+        int codNumero = Integer.parseInt(JOptionPane.showInputDialog("Informe o ID a ser deletedo: "));
+        NotaFiscalDAO dao = new NotaFiscalDaoJDBC();
+        dao.delete(codNumero);
+    }
+}
