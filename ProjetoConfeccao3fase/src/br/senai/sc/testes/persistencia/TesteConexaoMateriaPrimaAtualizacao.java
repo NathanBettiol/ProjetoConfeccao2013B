101e7ae57ade477a252c29f1dbfdf4e55afd4521
Proprietario <Proprietario@172.20.48.218>
2013-09-20 12:53:18 -0300

    Classe responsável pelo teste de atualização de uma materia-prima na base de dados.

--- ProjetoConfeccao3fase/src/br/senai/sc/testes/persistencia/TesteConexaoMateriaPrimaAtualizacao.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/persistencia/TesteConexaoMateriaPrimaAtualizacao.java
@@ -1,9 +1,9 @@
-
 package br.senai.sc.testes.persistencia;
 
 import br.senai.sc.model.negocio.CategoriaMateriaPrima;
 import br.senai.sc.model.negocio.MateriaPrima;
 import br.senai.sc.model.persistencia.CategoriaMateriaPrimaDaoJDBC;
+import br.senai.sc.model.persistencia.MateriaPrimaDaoJDBC;
 import br.senai.sc.persistencia.dao.CategoriaMateriaPrimaDAO;
 import br.senai.sc.persistencia.dao.MateriaPrimaDAO;
 import javax.swing.JOptionPane;
@@ -11,11 +11,13 @@ import javax.swing.JOptionPane;
 /**
  *
  * @author leticia_jesus
- * @version 1.0 20/09/2013
+ * @version 1.0 20/09/2013 
+ * 
  * Classe responsável pelo teste de atualização de uma materia-prima na base de dados.
  */
 public class TesteConexaoMateriaPrimaAtualizacao {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         MateriaPrima mp = new MateriaPrima();
 
         mp.setNome(JOptionPane.showInputDialog("Informe o nome "
@@ -23,13 +25,11 @@ public class TesteConexaoMateriaPrimaAtualizacao {
         mp.setPreco(Double.parseDouble(JOptionPane.showInputDialog("Informe o preço da matéria prima:")));
         mp.setTpUnidade(JOptionPane.showInputDialog("Informe o tipo das unidades de materia-prima:"));
         mp.setQuantidade(Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de materia-prima:")));
-        mp.setFornecedor()
-        mp.setCod(Integer.parseInt(JOptionPane.showInputDialog("Informe o "
+//        mp.setFornecedor(JOptionPane.showInputDialog("Informe o fornecedor da matéria-prima:"));
+        mp.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Informe o "
                 + "codigo da categoria da materia-prima a ser alterada")));
 
         MateriaPrimaDAO dao = new MateriaPrimaDaoJDBC();
         dao.update(mp);
     }
 }
-    
-
