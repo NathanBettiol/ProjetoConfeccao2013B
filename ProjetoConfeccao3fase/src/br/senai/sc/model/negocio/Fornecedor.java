b5de4143fea86b91eefa136b0173b504d8488927
giliard_j_carvalho <giliard_j_carvalho@10.1.48.180>
2013-09-20 14:18:20 -0300

    Alterado a classe Fornecedor.

--- ProjetoConfeccao3fase/src/br/senai/sc/model/negocio/Fornecedor.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/model/negocio/Fornecedor.java
@@ -9,7 +9,95 @@ package br.senai.sc.model.negocio;
  * @author leticia_jesus
  */
 public class Fornecedor {
-    public int cod_fornecedor;
-    public String nome;
-    public String cnpj;
+    private int codFornecedor;
+    private String nome;
+    private String cnpj;
+    private String endereco;
+    private String razaoSocial;
+    private String telefone;
+    private String email;
+    private String paginaWeb;
+    private String contato;
+    private CategoriaFornecedor categoriaFornecedor;
+
+    public int getCod_fornecedor() {
+        return codFornecedor;
+    }
+
+    public void setCod_fornecedor(int cod_fornecedor) {
+        this.codFornecedor = cod_fornecedor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPaginaWeb() {
+        return paginaWeb;
+    }
+
+    public void setPaginaWeb(String paginaWeb) {
+        this.paginaWeb = paginaWeb;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public CategoriaFornecedor getCategoriaFornecedor() {
+        return categoriaFornecedor;
+    }
+
+    public void setCategoriaFornecedor(CategoriaFornecedor categoriaFornecedor) {
+        this.categoriaFornecedor = categoriaFornecedor;
+    }
+    
 }
