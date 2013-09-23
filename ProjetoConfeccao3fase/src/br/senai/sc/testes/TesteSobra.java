88c2c4d697bd7e594621225b366697797e7ea13e
Owner <Owner@Computer-0668>
2013-09-23 19:23:01 -0300

    Alterando a data Cadastro para o tipo date

--- ProjetoConfeccao3fase/src/br/senai/sc/testes/TesteSobra.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/TesteSobra.java
@@ -1,6 +1,7 @@
 package br.senai.sc.testes;
 
 import br.senai.sc.model.negocio.Sobra;
+import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 
 /**
@@ -16,7 +17,14 @@ public class TesteSobra {
         s1.setPeso(JOptionPane.showInputDialog("Informe o peso" + s1.getPeso()));
         s1.setCategoria(JOptionPane.showInputDialog("Informe a categoria" + s1.getCategoria()));
         s1.setMateriaPrima(JOptionPane.showInputDialog("Informe a matéria prima" + s1.getMateriaPrima()));
-        s1.setDtCadastro(JOptionPane.showInputDialog("Informe a data de cadastro" + s1.getDtCadastro()));
+        try {
+                    String data = JOptionPane.showInputDialog("Data Cadastro");
+
+                    s1.setDtCadastro(new SimpleDateFormat("dd/mm/yyyy").parse(data));
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Data incorreta" + ex);
+                }
+        
         
         //Instanciando a segunda Sobra
         
@@ -24,7 +32,13 @@ public class TesteSobra {
         s2.setPeso(JOptionPane.showInputDialog("Informe o peso" + s2.getPeso()));
         s2.setCategoria(JOptionPane.showInputDialog("Informe a categoria" + s2.getCategoria()));
         s2.setMateriaPrima(JOptionPane.showInputDialog("Informe a matéria prima" + s2.getMateriaPrima()));
-        s2.setDtCadastro(JOptionPane.showInputDialog("Informe a data de cadastro" + s2.getDtCadastro()));
+        try {
+                    String data = JOptionPane.showInputDialog("Data Cadastro");
+
+                    s2.setDtCadastro(new SimpleDateFormat("dd/mm/yyyy").parse(data));
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Data incorreta" + ex);
+                }
         
         
     }
