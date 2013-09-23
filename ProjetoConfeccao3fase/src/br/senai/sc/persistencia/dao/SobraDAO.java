33d053101d23d9afdd0436cdd665968b30713c31
Owner <Owner@Computer-0668>
2013-09-23 01:08:49 -0300

    Inserção da classe de interface sobra com os métodos de inserção, atualização, deleção, listagem e listagem por id.

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/persistencia/dao/SobraDAO.java
@@ -0,0 +1,61 @@
+package br.senai.sc.persistencia.dao;
+
+import br.senai.sc.model.negocio.Sobra;
+import java.util.List;
+
+/**
+ *
+ * @author Patricia Gageiro
+ * @version 1.0 22/09/2013
+ * 
+ */
+public interface SobraDAO {
+    /*
+     * Método responsável pela inserção de uma sobra.
+     * @version 1.0 22/09/2013
+     * @author Patricia Gageiro
+     * @param p
+     * @return
+     * 
+     */
+    boolean insert(Sobra sob);
+
+    /*
+     * Método responsável pela atualização de uma sobra
+     * @version 1.0 22/09/2013
+     * @author Patricia Gageiro
+     * @param p
+     * @return
+     * 
+     */
+    boolean update(Sobra sob);
+
+
+    /*
+     * Método responsável pela deleção de uma sobra.
+     * @version 1.0 22/06/2013
+     * @author Patricia Gageiro
+     * @param p
+     * @return
+     */
+    boolean delete(Sobra sob);
+
+    /*
+     * Método responsável pela listagem das Sobras.
+     * @version 1.0 22/09/2013
+     * @author Patricia Gageiro
+     * @param p
+     * @return
+     */
+    List<Sobra> ListAll();
+    
+    /*
+     * Método responsável pela listagem por id da Sobras.
+     * @version 1.0 22/09/2013
+     * @author Patricia Gageiro
+     * @param p
+     * @return
+     */
+    Sobra listById(int id);
+}
+
