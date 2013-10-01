d3f7301b802dd37e5554925b3fd3c8252b4422fc
patricia_gageiro <patricia_gageiro@10.1.48.227>
2013-10-01 15:41:11 -0300

    Alterado o teste funcionario inserção

--- ProjetoConfeccao3fase/src/br/senai/sc/testes/persistencia/TesteConexaoFuncionarioInsercao.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/persistencia/TesteConexaoFuncionarioInsercao.java
@@ -23,6 +23,17 @@ public class TesteConexaoFuncionarioInsercao {
 
         Funcionario fun = new Funcionario();
         fun.setCod(1);
+        fun.setNome(JOptionPane.showInputDialog("Nome: "));
+        fun.setCpf(JOptionPane.showInputDialog("Cpf: "));
+        fun.setRg(JOptionPane.showInputDialog("Rg: "));
+         try {
+            String data = JOptionPane.showInputDialog("Data Nascimento");
+
+            fun.setDtNascimento(new SimpleDateFormat("dd/mm/yyyy").parse(data));
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Data incorreta" + ex);
+        }
+
         fun.setLogin(JOptionPane.showInputDialog("Login: "));
         fun.setEmail(JOptionPane.showInputDialog("Email: "));
         fun.setCtps(JOptionPane.showInputDialog("Ctps: "));
@@ -43,6 +54,7 @@ public class TesteConexaoFuncionarioInsercao {
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, "Data incorreta" + ex);
         }
+         
 
 
 
