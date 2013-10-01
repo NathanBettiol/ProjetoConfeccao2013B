80be73d0b907d5b9f4ec3c4d2c7c4fa7aa55ab63
douglas_ghisleri <douglas_ghisleri@10.1.48.255>
2013-10-01 13:54:37 -0300

    MetaDao

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/persistencia/dao/MetaDAO.java
@@ -0,0 +1,59 @@
+package br.senai.sc.persistencia.dao;
+
+import br.senai.sc.model.negocio.Meta;
+import java.util.List;
+
+/**
+ *
+ * @author douglas_ghisleri
+ */
+public interface MetaDAO {
+    /**
+     * Método responsável pela inserção de um endereço 
+     * na base de dados
+     * @author Rosicléia Frasson
+     * @version 1.0 09/09/13
+     * @param m
+     * @return 
+     */
+    boolean insert(Meta m);
+    
+    /**
+     * Método responsável pela atualização de um endereço
+     * na base de dados
+     * @author Rosicléia Frasson
+     * @version 1.0 09/09/13
+     * @param m
+     * @return 
+     */
+    boolean update(Meta m);
+    
+    /**
+     * Método responsável pela remoção de um endereço na
+     * base de dados
+     * @author Rosicléia Frasson
+     * @version 1.0 09/09/13
+     * @param cod_meta
+     * @return 
+     */
+    boolean delete (int cod_meta);
+    
+    /**
+     * Método responsável pela listagem de todas os
+     * endereços cadastrados na base de dados
+     * @author Rosicléia Frasson
+     * @version 1.0 09/09/13
+     * @return 
+     */
+    List<Meta> listAll();
+    
+    /**
+     * Método responsável por buscar um endereço na base de
+     * dados através de um id
+     * @author Rosicléia Frasson
+     * @version 1.0 09/09/13
+     * @param cod_meta
+     * @return 
+     */
+    Meta listById(int cod_meta);
+}
