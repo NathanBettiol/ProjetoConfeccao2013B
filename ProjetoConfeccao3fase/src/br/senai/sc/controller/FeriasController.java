b616b4b03f43b129d10625bdcdef4e6bf3a1b15b
larissa_mezari <larissa_mezari@10.1.48.225>
2013-10-28 17:00:58 -0200

    Criado férias controller

---
+++ ProjetoConfeccao3fase/src/br/senai/sc/controller/FeriasController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.controller;
+
+import br.senai.sc.model.negocio.Ferias;
+import br.senai.sc.model.persistencia.FeriasDaoJDBC;
+import br.senai.sc.persistencia.dao.FeriasDAO;
+import java.util.List;
+
+/**
+ *
+ * @author larissa_mezari
+ */
+public class FeriasController {
+      public List<Ferias> listAll() {
+        FeriasDAO dao = new FeriasDaoJDBC();
+        return dao.listAll();
+    }
+
+    public boolean insert (Ferias f){
+        FeriasDAO dao = new FeriasDaoJDBC();
+        return dao.insert(f);
+    }
+    
+    public boolean update (Ferias f){
+        FeriasDAO dao = new FeriasDaoJDBC();
+        return dao.update(f);
+    }
+    
+     public boolean delete (int codUsuario){
+        FeriasDAO dao = new FeriasDaoJDBC();
+        return dao.delete(codUsuario);
+    } 
+}
