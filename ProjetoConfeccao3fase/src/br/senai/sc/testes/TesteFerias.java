25fb638d0e84e402baf336d5615f46377c73b2e0
larissa_mezari <larissa_mezari@10.1.48.225>
2013-10-28 17:01:26 -0200

    Alterado classe para testar ferias

--- ProjetoConfeccao3fase/src/br/senai/sc/testes/TesteFerias.java
+++ ProjetoConfeccao3fase/src/br/senai/sc/testes/TesteFerias.java
@@ -16,44 +16,44 @@ import javax.swing.JOptionPane;
 public class TesteFerias {
     public static void main(String[] args) {
         
-    Ferias f1 = new Ferias();
-    f1.setFuncionario("Informe o nome do funcionario: " + f1.getFuncionario());
-    try {
-        	String data = JOptionPane.showInputDialog("Data de Cadastro: ");
-        	f1.setDataFim((Date) new SimpleDateFormat("dd/MM/yyyy").parse(data));
-    	} catch (Exception e) {
-        	JOptionPane.showMessageDialog(null, "Data incorreta: " + e);
-    	}
-    try {
-        	String data = JOptionPane.showInputDialog("Data de Cadastro: ");
-        	f1.setDataInicio((Date) new SimpleDateFormat("dd/MM/yyyy").parse(data));
-    	} catch (Exception e) {
-        	JOptionPane.showMessageDialog(null, "Data incorreta: " + e);
-    	}
+//    Ferias f1 = new Ferias();
+//    f1.setFerias("Informe o nome do funcionario: " + f1.getFerias());
+//    try {
+//        	String data = JOptionPane.showInputDialog("Data de Cadastro: ");
+//        	f1.setDataFim((Date) new SimpleDateFormat("dd/MM/yyyy").parse(data));
+//    	} catch (Exception e) {
+//        	JOptionPane.showMessageDialog(null, "Data incorreta: " + e);
+//    	}
+//    try {
+//        	String data = JOptionPane.showInputDialog("Data de Cadastro: ");
+//        	f1.setDataInicio((Date) new SimpleDateFormat("dd/MM/yyyy").parse(data));
+//    	} catch (Exception e) {
+//        	JOptionPane.showMessageDialog(null, "Data incorreta: " + e);
+//    	}
+//    
+//    
+//    
+//    
+//    Ferias f2 = new Ferias();
+//    f2.setFerias("Informe o nome do funcionario: " + f1.getFerias());
+//    try {
+//        	String data = JOptionPane.showInputDialog("Data de Cadastro: ");
+//        	f2.setDataFim((Date) new SimpleDateFormat("dd/MM/yyyy").parse(data));
+//    	} catch (Exception e) {
+//        	JOptionPane.showMessageDialog(null, "Data incorreta: " + e);
+//    	}
+//   try {
+//        	String data = JOptionPane.showInputDialog("Data de Cadastro: ");
+//        	f2.setDataInicio((Date) new SimpleDateFormat("dd/MM/yyyy").parse(data));
+//    	} catch (Exception e) {
+//        	JOptionPane.showMessageDialog(null, "Data incorreta: " + e);
+//    	}
+//    
+//  
+//  
+//            }
     
-    
-    
-    
-    Ferias f2 = new Ferias();
-    f2.setFuncionario("Informe o nome do funcionario: " + f1.getFuncionario());
-    try {
-        	String data = JOptionPane.showInputDialog("Data de Cadastro: ");
-        	f2.setDataFim((Date) new SimpleDateFormat("dd/MM/yyyy").parse(data));
-    	} catch (Exception e) {
-        	JOptionPane.showMessageDialog(null, "Data incorreta: " + e);
-    	}
-   try {
-        	String data = JOptionPane.showInputDialog("Data de Cadastro: ");
-        	f2.setDataInicio((Date) new SimpleDateFormat("dd/MM/yyyy").parse(data));
-    	} catch (Exception e) {
-        	JOptionPane.showMessageDialog(null, "Data incorreta: " + e);
-    	}
-    
-  
-  
-            }
-    
-    
+    } 
     
     
     
